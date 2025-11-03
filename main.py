78a4fa0840b676e254c8470578bce8c1aae846b0
Baods01 <1433573081@qq.com>
2025-11-03 22:57:24 +0800

    第五次存档

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,APIRouter
+import uvicorn
 
-@app.get("/")
+app01 = APIRouter()
+
+@app01.get("/")
 async def get():
     return {"message": "Hello World"}
 
-print("hello world")
+app = FastAPI()
+app.include_router(app01,tags=["App01"])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",port=8080,reload=True)
